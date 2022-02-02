21a392bb75c92e7bea3e722bfbece9809a68ae9f
ROSHINISINGH07 <98823832+ROSHINISINGH07@users.noreply.github.com>
2022-02-02 15:21:59 +0530

    Add files via upload
    y = input('enter the character')
    
    if(y == 's') or (y == 'i') or (y == 'c') or (y == 'a') or (y == 'n'):
        print('Congratulations you won the lottery')
    
    else:
        print('Better luck next time')

---
+++ ASSIGNMENT2/main.py
@@ -0,0 +1,8 @@
+
+y = input('enter the character')
+
+if(y == 's') or (y == 'i') or (y == 'c') or (y == 'a') or (y == 'n'):
+    print('Congratulations you won the lottery')
+
+else:
+    print('Better luck next time')
